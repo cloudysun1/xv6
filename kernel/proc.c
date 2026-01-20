3985da9f3e744980c65987170c89859d97251b34
Haotian <ccsuen35@gmail.com>
2026-01-20 12:34:28 +0100

    gdb

--- kernel/proc.c
+++ kernel/proc.c
@@ -288,7 +288,8 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
-
+extern uint64 sys_uptime(void);
+  np->trace_mask = p->trace_mask;
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
